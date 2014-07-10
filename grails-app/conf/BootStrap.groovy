8d06b2d1fe7b61de5d2b3a32ac520dbd94defc48
Margus Pärt <margus@wave.ee>
2014-07-10 04:02:04 +0300

    Automatically create basic data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,25 @@
 class BootStrap {
 
     def init = { servletContext ->
+        if(klass.Triple.count() > 0) return false
+
+        def triples_data = [
+            ["Color", "type", "Class"],
+            ["Red", "is a", "Color"],
+            ["Green", "is a", "Color"],
+            ["Car", "type", "Class"],
+            ["Car", "has one", "Color"],
+            ["Ivani auto", "is a", "Car"],
+            ["Ivani auto", "has color", "Red"]
+        ]
+
+        triples_data.collect {
+            new klass.Triple(subject: it[0], predicate: it[1], object: it[2]).save()
+        }
+
     }
+
     def destroy = {
     }
+
 }
